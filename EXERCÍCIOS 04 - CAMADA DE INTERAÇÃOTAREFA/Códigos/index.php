94ff1cdc1b55e4d9c678a4178eb686b591daa970
Marcos Veniciu <marcos.barbalho@ufv.br>
2022-07-21 11:32:48 -0300

    Pagina de login

---
+++ EXERCÍCIOS 04 - CAMADA DE INTERAÇÃOTAREFA/Códigos/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Locadora Lascou-se</title>
+</head>
+<header class="site-header">
+    <div class="site-title">
+        <a href="#">Locadora Lascou-se</a>
+    </div>
+    <div>
+        <nav>
+            <button onclick="" class="signin">
+                <a href="#">Logar</a>
+            </button>
+            <button onclick="" class="signup">
+                <a href="#">Cadastrar</a>
+            </button>
+        </nav>
+    </div>
+</header>
+<body>
+    
+</body>
+</html>
